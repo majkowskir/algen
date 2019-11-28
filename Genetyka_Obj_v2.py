e15fcb2acf3b0c61ffbd1620d09301c026c7afa7
majkowskir <majkowski.r@gmail.com>
2019-11-28 15:59:05 +0100

    AlGen Obj - dziala

--- Genetyka_Obj_v2.py
+++ Genetyka_Obj_v2.py
@@ -4,10 +4,13 @@
 
 import time, math, matplotlib
 import matplotlib.pyplot as plt
+
 import numpy as np
 import random as rand
 
-from collections import Counter
+#from collections import Counter
+
+from operator import itemgetter
 
 import tkinter as tk
 from tkinter import *
@@ -23,7 +26,7 @@ iteracja = 0 # wartość startowa, warunek dla iteracja = Gen
 Gen = 50
 
 # pop_size - liczebność populacji, dobrze, aby była parzysta
-pop_size = 300
+pop_size = 150
 
 # prawdopodobieństwa: krzyżowania (Pc) oraz mutacji (Pm)
 Pc = 0.25
@@ -66,26 +69,27 @@ class Osobnik:
 		v_chromosom = np.random.choice(a=[0, 1], size=m)
 		self.chromosom = v_chromosom
 
-	def mutacja(self):
-		pozycja_mutacji = rand.randint(0, m-1)
-		mutant = abs(self.chromosom[pozycja_mutacji]-1)
-		#return (mutant, pozycja_mutacji)
-		return (mutant)
+	def mutacja(self, rodzic_1):
+		pass
 
 	def kopiowanie(self):
-		return self.chromosom
+		pass
+		#return self.chromosom
 
-	def crossover(self, pula):
+	def crossover(self, rodzic_1, rodzic_2):
 		punkt_krzyzowania = rand.randint(1, m-2) # punkt krzyżowania, przecięcie chromosomu najwcześniej za pierwszym bitem, najpóźniej za przedostatnim
-		rodzic_2 = rand.randint(0, pop_size-1)
-	
-		parent_2 = pula.stado[rodzic_2].chromosom
-		dziecko_1 = np.concatenate((self.chromosom[:punkt_krzyzowania], parent_2[punkt_krzyzowania:]), axis=None)   
-		dziecko_2 = np.concatenate((parent_2[:punkt_krzyzowania], self.chromosom[punkt_krzyzowania:]), axis=None)
+		
+		#print("Rodzic 1: %s" % rodzic_1)
+		#print("Rodzic 2: %s" % rodzic_2)
+		# print("Punkt krzyzowania: %s" % punkt_krzyzowania)
 
-		self.chromosom = dziecko_1
-		#pula.stado[rodzic_2].chromosom = dziecko_2
-		#return (self.chromosom, parent_2, punkt_krzyzowania, dziecko_1, dziecko_2) # lista dla dwóch zwracanych wartości
+		try:
+			dziecko_1 = np.concatenate((rodzic_1[:punkt_krzyzowania], rodzic_2[punkt_krzyzowania:]), axis=None)   
+			dziecko_2 = np.concatenate((rodzic_2[:punkt_krzyzowania], rodzic_1[punkt_krzyzowania:]), axis=None)
+		except:
+			print("blad krzyzowania")
+		# print("Dziecko 1: %s" % dziecko_1)
+		# print("Dziecko 2: %s" % dziecko_2)
 		return (dziecko_1, dziecko_2)
 		
 
@@ -100,11 +104,10 @@ class Populacja:
 	    wartosci_f = [] # usunięcie danych z listy do przechowywania wartosci funkcji dla danego pokolenia
 	    for i in range(pop_size):
 	        my_lst = self.stado[i].chromosom
-	        str1=""
-	        str1 = "".join(map(str, my_lst)) # łączenie elementów listy w string
+        	str1=""
+	       	str1 = "".join(map(str, my_lst)) # łączenie elementów listy w string
 	        dekodowanie2dec = int(str1, 2) # dekodowanie binarki do liczby dziesiętnej 
 	        argument = ((Xmax-Xmin)*dekodowanie2dec)/((2**m)-1)+Xmin # mapowanie chromosomu do wartości x z zakresu (Xmin,Xmax)
-
 	        wartosc = funkcja(argument) # wartość funkcji w punkcie x 
 	        if wartosc < 0: wartosc = 0 # zapewniamy dla ruletki wartości dodatnie przez wyzerowanie ujemnych
 
@@ -126,59 +129,69 @@ class Populacja:
 
 		sektor = 0
 		kolo_ruletki = []
-		for i in range(pop_size):
+		for i in range(len(Ps)):
 			sektor = sektor + Ps[i]
 			kolo_ruletki.append(sektor) # dodawanie do listy wartości brzegowej sektora
 
-		pula_rodzicielska = []
-		for i in range(pop_size): 
-			losowa = rand.random() # losujemy liczbę z przedziału (0,1) tyle razy, ile osobników w populacji
-			sektor = 0 # zaczynamy od sektora zero
-			for j in range(len(kolo_ruletki)): # sprawdzamy, do którego sektora na kole ruletki wpadła wylosowana liczba
-				if losowa > kolo_ruletki[j]:
-					sektor=sektor+1
-			pula_rodzicielska.append(self.stado[sektor].chromosom) # do puli rodzicielskiej dodajemy osobnika z puli zero
-		
-		# nadpisanie stada pulą rodzicielską
-		for i in (range(len(pula_rodzicielska))):
-			self.stado[i].chromosom = pula_rodzicielska[i]
-		return(F)
+		losowa = rand.uniform(0,(kolo_ruletki[len(kolo_ruletki)-1])) # losujemy liczbę z przedziału (0, 1 lub czasami do 0,99999999 ) 
+		sektor = 0 # zaczynamy od sektora zero
+		for j in range(len(kolo_ruletki)): # sprawdzamy, do którego sektora na kole ruletki wpadła wylosowana liczba
+			if losowa > kolo_ruletki[j]:
+				sektor = sektor+1
+		return(self.stado[sektor].chromosom)
 
-	def operacje(self, pula):
+	def operacje(self):
 		potomstwo = []
+		iteracja = iter(range(len(self.stado)))
 
-		stado = iter(range(len(pula)))
-		for i in stado:
+		for i in iteracja:
 			operacja = rand.random()
 			if operacja > (Pc+Pm):
-				potomstwo.append(self.stado[i].kopiowanie())
+				try:
+					potomstwo.append(self.ruletka())
+				#	print(i, "\tIteracja operacyjna: kopiowanie")
+				except:
+					print("Błąd kopiowania!")
 			elif operacja > Pm:
-				potomstwo.append(self.stado[i].crossover(pula))
-				next stado
+				rodzic_1 = self.ruletka()
+				rodzic_2 = self.ruletka()
+				dzieci = self.stado[i].crossover(rodzic_1, rodzic_2)
+				try:
+					next(iteracja)
+					potomstwo.append(dzieci[0]) # tu jest brzydko, bo na twardo, powinna być jakaś pętla
+					potomstwo.append(dzieci[1])
+				except:
+					potomstwo.append(dzieci[0]) # jeśli krzyzujemy ostatniego osobnika w puli, propagowane jest tylko jedno dziecko
+				#print(i, "\tIteracja operacyjna: krzyżowanie")
 			else:
-				potomstwo.append(self.stado[i].mutacja())
-			next stado
-		return()
+				#print("mutowanie \n")
+				pozycja_mutacji = rand.randint(0, m-1)
+				rodzic_1 = self.ruletka()
+				#print("Rodzic 1:", rodzic_1)
+				#print("Pozycja mutacji: ", pozycja_mutacji)
+				rodzic_1[pozycja_mutacji]=abs(rodzic_1[pozycja_mutacji]-1)
+				mutant = rodzic_1
+				potomstwo.append(mutant)
+				#print(i, "\tIteracja operacyjna: mutacja")
 
 
+		# przepisanie stada rodzicielskiego na potomne:
+		for i in (range(len(potomstwo))): 
+			#print(i,self.stado[i].chromosom," ",potomstwo[i])
+			potomek = potomstwo[i]
+			self.stado[i].chromosom = potomek
+		return(potomstwo)
 
 Stado_Alfa = Populacja(pop_size) # utworzenie stada A
 
 # wypisanie osobników w stadzie A
-for i in range(pop_size):
-	print(Stado_Alfa.stado[i].chromosom)
-
-epoki = iter(range(Gen))
-for i in epoki:
-	Stado_Alfa.ewaluacja()
-	Stado_Alfa.ruletka()
-	print(round(Stado_Alfa.ruletka()/pop_size,d))
-	Stado_Alfa.operacje(Stado_Alfa)
-
-
-# print("Po x iteracjach")
 # for i in range(pop_size):
 # 	print(Stado_Alfa.stado[i].chromosom)
 
-#print(*Stado_Alfa.ewaluacja(), sep="\n")
-#print(*wartosci_funkcji_do_wykresu, sep="\n")
+for iteracja in range(Gen):
+	Stado_Alfa.operacje()
+	wartosci = Stado_Alfa.ewaluacja()
+	F = [sum(i) for i in zip(*wartosci)]
+	print("Pokolenie %s, stado: %s, średni_argument: %s" % (iteracja, len(Stado_Alfa.stado), (round(F[0]/len(Stado_Alfa.stado),d))),", średnia wartosc f(x) w pokoleniu: ",(round(F[1]/len(Stado_Alfa.stado),d)))
+
+print(max(wartosci, key=itemgetter(1))[0])
